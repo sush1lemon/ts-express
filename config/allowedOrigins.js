69d14c77feda1d5cae63ce538619e20601e50c27
jezerdave <jezerdavebacquian@gmail.com>
2022-05-12 16:07:33 +0800

    add origin

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const allowedOrigins = [
-    'http://localhost:3000'
+    'http://localhost:3000',
+    'https://react-project-ts.pages.dev'
 ];
 exports.default = allowedOrigins;
